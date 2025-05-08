8a813769c8d9c7d928fc60038b0b041e92ee6643
saianumala <anumalansk@gmail.com>
2025-05-08 12:36:03 +0530

    added activity route

--- src/routes/activityRoute.ts
+++ src/routes/activityRoute.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { userAuthorization } from "../middleware/userAuth";
+import {
+  createActivity,
+  deleteActivity,
+  getActivity,
+  getAllActivities,
+  updateActivity,
+} from "../controllers/activityController";
 
 const router = Router();
+
+router.post("/create", userAuthorization, createActivity);
+router.patch("/update/:id", userAuthorization, updateActivity);
+router.delete("/delete/:id", userAuthorization, deleteActivity);
+router.get("/all", userAuthorization, getAllActivities);
+router.get("/:id", userAuthorization, getActivity);
 
 export default router;
