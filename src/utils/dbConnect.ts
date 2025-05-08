84a7e0b5a57cb0f6356b395a6d17efd963216285
saianumala <anumalansk@gmail.com>
2025-05-08 11:28:58 +0530

    added db connection

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 async function DBConnect() {
   try {
-    const dbconnection = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${process.env.DB_NAME}`
-    );
+    const dbconnection = await mongoose.connect(`${process.env.MONGODB_URL}`);
     console.log(`MondoDb connected securely: ${dbconnection.connection.host}`);
   } catch (error) {
     console.log("mongodb faied to connect: ", error);
