3cff0a7c6c63a6b16c8f57fc9c007e3ae2372f8e
saianumala <anumalansk@gmail.com>
2025-05-08 11:26:41 +0530

    added user route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
-
+import { userAuthorization } from "../middleware/userAuth";
+import {
+  register,
+  deleteUser,
+  getUser,
+  login,
+  logout,
+  updateUser,
+} from "../controllers/userController";
 const router = Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", userAuthorization, logout);
+router.patch("/update", userAuthorization, updateUser);
+router.delete("/delete", userAuthorization, deleteUser);
+router.get("/:id", getUser);
 
 export default router;
